8803699756644720e917f11e087f5253314fb982
Mauricio Sauza <mauricio.sauza@trainig.assuresoft.com>
2023-06-27 16:04:32 -0400

    exception code corrected

---
+++ src/main/java/com/ad/backend/CarSystemApi/Exceptions/ErrorResponse.java
@@ -0,0 +1,16 @@
+
+package com.ad.backend.CarSystemApi.Exceptions;
+
+import lombok.Data;
+
+@Data
+public class ErrorResponse {
+    private int status;
+    private String message;
+
+    public ErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
+

b7d6e6a1b5d15e0a96e2777c34290de8a4966156
luisperez10as <luis.perez@training.assuresoft.com>
2023-06-27 23:18:35 -0400

    Local Changes

--- src/main/java/com/ad/backend/CarSystemApi/Exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ad.backend.CarSystemApi.Exceptions;
-
-public class UserNotFoundException extends Exception {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/ad/backend/CarSystemApi/repositories/ICarRepository.java
+++ src/main/java/com/ad/backend/CarSystemApi/repositories/ICarRepository.java
@@ -3,6 +3,6 @@ package com.ad.backend.CarSystemApi.dao;
 import com.ad.backend.CarSystemApi.models.Car;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ICarDao extends CrudRepository<Car, Long> {
+public interface CarRepository extends CrudRepository<Car, Long> {
 
 }
